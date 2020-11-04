7e1d3eeeffab9164fdba2f04343021420a8dea5c
lod_ash <lobanov.e@oulook.com>
2020-11-04 12:33:48 +0300

    autoloader with namespace

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 
-spl_autoload_register(function ($class_name) {
-    include_once "controllers/$class_name.php";
-});
+function autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-$route = $_GET['route'] ?? '';
-$matches = explode('/', $route);
+    require $fileName;
+}
+spl_autoload_register('autoload');
+define('ROOT', dirname(__FILE__));
+define('CONFIG_DIR_PATH', ROOT . '/configs');
 
-$controller = $matches[0] ?? '';
-$controller .= 'Controller';
-$action = 'action';
-$action .= ucfirst($matches[1] ?? 'index');
-
-
-$oController = new $controller();
-echo $oController->$action();
+$oController = new controllers\Controller();
+$oController->route();
