8e05d2fa79b6577d12ab6162691bbe37c30f5beb
lod_ash <lobanov.e@oulook.com>
2020-11-08 20:23:38 +0300

    split templates phone

--- Views/template/phone/list.php
+++ Views/template/phone/list.php
@@ -17,19 +17,19 @@ use components\Validator as Validator; ?>
         </div>
 
         <div class="my-3 p-3 bg-white rounded shadow-sm">
-
-            <button type="button" class="btn btn-primary btn-sm js-add_new">
-                <span class="glyphicon glyphicon-pencil"></span> Добавить нового
-            </button>
-
+            <div>
+                <button type="button" class="btn btn-primary btn-sm js-add_new">
+                    <span class="glyphicon glyphicon-pencil"></span> Добавить нового
+                </button>
+            </div>
             <div class="js-list">
                 <?php foreach ($aList as $aItem): ?>
-                    <?php include ROOT . '/views/template/_item_full.php'; ?>
+                    <?php include ROOT . '/views/template/phone/_item_full.php'; ?>
                 <?php endforeach; ?>
             </div>
         </div>
     </main>
 
-<?php include_once ROOT . '/views/template/_form_add.php'; ?>
+<?php include_once ROOT . '/views/template/phone/_form_add.php'; ?>
 
 <?php include ROOT . '/views/layouts/footer.php'; ?>
